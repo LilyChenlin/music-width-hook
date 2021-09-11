bce5be3ce27576479e761e979fe83c099cdf7e7a
LilyChenlin <906791425@qq.com>
2021-09-11 14:49:18 +0800

    update .gitignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import _ from 'lodash';
+import print from './print.js';
+
+
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], '');
-    
+    btn.innerHTML = 'Please click me';
+    btn.onclick = print;
+
+    element.appendChild(btn);
     return element;
 }
 document.body.appendChild(component())
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log("Accepting the updated printMe module!");
+        print();
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        // print: './src/print.js'
+        // hot: 'webpack/hot/dev-server.js',
+        // client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        hot: true,
+        // client: false
+    },
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        // cllTodo: output中开启clean:true 功能等价于clean-webpack-plugin? 是webpack5的特性吗
+        clean: true, // 功能等价于clean-webpack-plugin? 是webpack5的特性吗
+    },
+    module: {
+        rules: [
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        }),
+        // new webpack.HotModuleReplacementPlugin(),
+    ]
 }
