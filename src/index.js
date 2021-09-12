4e3163e4bb1c814c43247b90ff0b14610278d764
LilyChenlin <906791425@qq.com>
2021-09-12 17:43:04 +0800

    feat: 构建产物js文件添加唯一值hash，保证缓存机制，优化资源

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import _ from 'lodash';
+// import Print from './print';
 function component() {
     const element = document.createElement('div');
-    const button = document.createElement('button');
-    const br = document.createElement('br');
 
-    button.innerHTML = 'Click me and look at the console';
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.appendChild(button);
-    element.appendChild(br);
+    // element.onclick = Print.bind(null, 'Hello webpack')
 
-    button.onclick = e => import(/* webpackChunkName: "print" */ './print').then((module) => {
-        const print = module.default;
-        print();
-    })
     return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -26,23 +26,32 @@ module.exports = {
         // client: false
     },
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         // cllTodo: output中开启clean:true 功能等价于clean-webpack-plugin? 是webpack5的特性吗
         clean: true, // 功能等价于clean-webpack-plugin? 是webpack5的特性吗
     },
-    // optimization: {
-    //     splitChunks: {
-    //         chunks: 'all'
-    //     }
-    // },
+    optimization: {
+        // 提取引导模版代码
+        runtimeChunk: 'single',
+        moduleIds: 'deterministic',// 添加该配置用于解决每次在代码中引入新的模块/删除新的模块引入时，打包的vendors都会生成一个新的hash值，但是这个hash的重复生成是多余的
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
     module: {
         rules: [
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development'
+            title: 'caching',
         }),
         // new webpack.HotModuleReplacementPlugin(),
     ]
