eb066360f4098e4f2543f9b1c7f6c8b9e8edfd88
LilyChenlin <906791425@qq.com>
2022-05-11 11:27:43 +0800

    refactor(RecommendList): TS重构

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import Lazyload from 'react-lazyload';
-import { withRouter } from 'react-router-dom';
+import LazyLoad from 'react-lazyload';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import {
     ListWrapper,
     ListItem,
     List
 } from './style'
-import {getCount} from '../../api/util'
-const RecommendList = (props) => {
-    const enterDetail = (id) => {
-        props.history.push (`/recommend/${id}`)
+import { getCount } from '../../api/util';
+import { IRecommendList } from '../../application/Recommend/store/reducer';
+
+interface RecommendListProps extends RouteComponentProps {
+    history: any;
+    recommendList: IRecommendList;
+}
+const RecommendList: React.FC<RecommendListProps> = ({ history, recommendList }) => {
+    console.log("recommendList", recommendList);
+    const enterDetail = (id: string) => {
+        history.push (`/recommend/${id}`)
     }
     return(
         <ListWrapper>
             <h1 className="title">推荐歌单</h1>
             <List>
                 {
-                    props.recommendList.map((item, index) => {
+                    recommendList.map((item, index) => {
                         return (
                             <ListItem key={index} onClick={() => enterDetail(item.id)}>
                                 <div className="img_wrapper">
                                     <div className="decorate"></div>
                                     {/* 在img外面加一层Lazyload图片懒加载 */}
-                                    <Lazyload placeholder={<img width="100%" height="100%" src={require('./music.png')}/>}>
+                                    <LazyLoad placeholder={<img width="100%" height="100%" src={require('./music.png')} alt="music"/>}>
                                         <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music"/>
-                                    </Lazyload>
+                                    </LazyLoad>
                                     {/* 加此参数可以减小请求的图片资源大小 */}
                                     <div className="play_count">
                                         <i className="iconfont play">&#xe63a;</i>
